bedce0ad8952a882e0ab58694ab09a64bd147d91
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-03 21:26:55 +0300

    Solved Valid Anagram Array Problem on NeetCode

---
+++ Arrays/Array-ez-ValidAnagram/Array-ez-ValidAnagram/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Array_ez_ValidAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "andd";
+            string t = "dadd";
+            Solution solution = new Solution();
+            Console.WriteLine( solution.IsAnagram(s, t) );
+
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            Dictionary<char,int> freq = new Dictionary<char, int>();
+            
+            if (s.Length != t.Length) { return false; }
+
+            foreach (char c in s)
+            {
+                if (freq.ContainsKey(c)) { freq[c]++; }
+                else { freq[c] = 1; }
+            }
+            foreach (char c in t)
+            {
+                if (!freq.ContainsKey(c)) { return false; }
+                if (freq.ContainsKey(c)) { freq[c]--; }
+                if (freq[c] == 0) { freq.Remove(c); }
+            }
+
+            if (freq.Count == 0) { return true; }
+            else return false;
+        }
+    }
+
+}
