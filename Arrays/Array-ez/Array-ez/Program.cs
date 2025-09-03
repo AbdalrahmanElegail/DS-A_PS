31890754f1a3bbf7f3c5c757bf33ce1cf1d4d4cf
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-03 20:51:37 +0300

    Solved Contains Duplicate Problem

--- Arrays/Array-ez/Array-ez/Program.cs
+++ Arrays/Array-ez/Array-ez/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Array_ez
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Array_ez
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = { 1, 2, 3, 5, 4, 6 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.hasDuplicate(nums));
+
+        }
+    }
+
+    public class Solution
+    {
+        public bool hasDuplicate(int[] nums)
+        {
+
+            HashSet<int> nums2 = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums2.Contains(nums[i])) { return true; } // duplicate found
+                else { nums2.Add(nums[i]); }
+            }
+            return false;
         }
     }
 }
