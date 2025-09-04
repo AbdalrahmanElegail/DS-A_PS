8ca289328c76835971b49923e85b6ee3500ea5b1
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-04 18:18:59 +0300

    Solved Top K Frequent Elements Array Problem

---
+++ Arrays/Array-mdm-TopKFrequentElements/Array-mdm-TopKFrequentElements/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Array_mdm_TopKFrequentElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] nums = { 1, 2 };
+            int k = 2;
+            Solution solution = new Solution();
+            int[] result = solution.TopKFrequent(nums, k);
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+    public class Solution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (frequencyMap.ContainsKey(num))
+                    frequencyMap[num]++;
+                else
+                    frequencyMap[num] = 1;
+            }
+
+            int[] result = frequencyMap
+                                .OrderByDescending(x => x.Value)
+                                .Take(k)
+                                .Select(x => x.Key)
+                                .ToArray();
+
+            return result;
+        }
+    }
+}
