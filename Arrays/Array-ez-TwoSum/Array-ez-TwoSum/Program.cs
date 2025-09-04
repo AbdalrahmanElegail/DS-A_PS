559fe66ead778c4ab0990ef0e6a3e7de476eb5e5
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-04 07:18:36 +0300

    Solved Two Sum Array Problem on NeetCode

---
+++ Arrays/Array-ez-TwoSum/Array-ez-TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Array_ez_TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 5, 6 };
+            int target = 10;
+            Solution solution = new Solution();
+            int[] result = solution.TwoSum(nums, target);
+            Console.WriteLine($"{string.Join(", ", result)}");
+
+        }
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                int j = Array.IndexOf(nums, (target - nums[i]));
+
+                if (j >= 0 && i != j)
+                {
+                    return new int[] { j, i };
+                }
+            }
+            return new int[0];
+        }
+    }
+}
