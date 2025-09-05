35f7045fdb1c988e118194e3f44602d484e454d9
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-05 12:20:02 +0300

    Solved Encode and Decode Strings Array Problem in a hacked way

---
+++ Arrays/Array-mdm-EncodeAndDecodeStrings/Array-mdm-EncodeAndDecodeStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Array_mdm_EncodeAndDecodeStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strs = new List<string> {};
+            Solution solution = new();
+            string? encoded = solution.Encode(strs);
+            List<string>? refactored = solution.Decode(encoded);
+
+            Console.WriteLine(string.Join("",refactored));
+
+        }
+    }
+
+
+    public class Solution
+    {
+
+        public string? Encode(IList<string> strs)
+        {
+            if (strs.Count == 0) {  return null; }
+
+            string result = string.Join('"', strs);
+            return result;
+        }
+
+        public List<string>? Decode(string s)
+        {
+            if (s==null) return new List<string> { };
+
+            List<string> result = s.Split('"').ToList();
+            return result;
+        }
+    }
+
+}
