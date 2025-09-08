1b2f87c132a0fc72597ad20dab9833f213c42d4d
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-08 08:47:23 +0300

    Solved Valid Parentheses Stack problem and Rename Arrays folder to be Arrays-Hashing

---
+++ Stack/Stack-ez-ValidParentheses/Stack-ez-ValidParentheses/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Stack_ez_ValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string input = "}";
+            bool result = solution.IsValid(input);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> chars = new Stack<char>();
+            foreach (char c in s)
+            {
+                if(c == '{' || c == '[' || c == '(')
+                    chars.Push(c); 
+                else if ( chars.Count != 0 && c == '}' && chars.Peek() == '{' ||
+                          chars.Count != 0 && c == ']' && chars.Peek() == '[' ||
+                          chars.Count != 0 && c == ')' && chars.Peek() == '(' )
+                    chars.Pop();
+                else
+                    return false;
+            }
+            return chars.Count == 0 ? true : false;
+        }
+    }
+
+
+}
