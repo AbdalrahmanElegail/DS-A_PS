70d2c37c4d4bb3912a372a4bffcff0f4e2ee57b3
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-11 16:07:23 +0300

    Solved Two Integer Sum II Two-Pointers Problem

---
+++ Two-Pointers/Two_Pointers_mdm-TwoIntegerSumII/Two_Pointers_mdm-TwoIntegerSumII/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Two_Pointers_mdm_TwoIntegerSumII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] result = solution.TwoSum(new int[] { 2,4   ,8,10,11,12, 15 }, 13);
+            Console.WriteLine($"{result[0]} , {result[1]}");
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int i = 0, j = numbers.Length - 1;
+            for (; i < j;)
+            {
+                int sum = numbers[i] + numbers[j];
+                if (sum < target) i++;
+                else if (sum > target) j--;
+                else break;
+            }
+            return new int[] {i + 1, j + 1};
+        }
+    }
+
+}
