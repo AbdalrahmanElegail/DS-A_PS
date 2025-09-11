7a4d7d64b78ea3fcc2986494e77bf79a340c51cf
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-11 14:43:30 +0300

    Solved Valid Palindrome Two-Pointers Problem

---
+++ Two-Pointers/Two-Pointers-ez-ValidPalindrome/Two-Pointers-ez-ValidPalindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Two_Pointers_ez_ValidPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "Was it a car or a cat I saw";
+            Console.WriteLine(solution.IsPalindrome(s));
+        }
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            int i=0, j=s.Length-1;
+            while (i <= j)
+            {
+                if (s[i] == ' ' || !char.IsLetterOrDigit(s[i]))
+                {
+                    i++;
+                    continue;
+                }
+                if (s[j] == ' ' || !char.IsLetterOrDigit(s[j]))
+                {
+                    j--;
+                    continue;
+                }
+                if (char.ToLower(s[i]) != char.ToLower(s[j])) { return false; }
+                else
+                {
+                    i++;
+                    j--;
+                }
+            }
+            return true;
+        }
+    }
+
+}
