a5acde99b4ad64d46bce469732aea38d32ff9d1b
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-16 15:55:41 +0300

    Solved Rotate Image Maths and Geometry Problem

---
+++ maths-Geometry/Maths-Geometry-ez-RotateImage/Maths-Geometry-ez-RotateImage/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Maths_Geometry_ez_RotateImage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[][] matrix = [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ];
+
+            s.Rotate(matrix);
+
+            for(int i = 0; i < matrix.Length; i++)
+            {
+                for(int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+
+    public class Solution
+    {
+        public void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+            for (int i= 0; i < (n+1)/2; i++)
+            {
+                for(int j = i; j < n - i - 1; j++)
+                {
+                    // save the top left position
+                    int temp = matrix[i][j];
+                    // move bottom left to top left
+                    matrix[i][j] = matrix[n - j - 1][i];
+                    // move bottom right to bottom left
+                    matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                    // move top right to bottom right
+                    matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                    // move top left to top right
+                    matrix[j][n - i - 1] = temp;
+                }
+            }
+        }
+    }
+
+}
