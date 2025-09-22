fa10274e34414682470a2b16771ca6c91313469b
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-22 11:22:30 +0300

    Solved Single Number Bit-Manipulation problem

---
+++ Bit-Manipulation/Bit-Manipulation-SingleNumber/Bit-Manipulation-SingleNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Bit_Manipulation_SingleNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = { 4, 1, 2, 1, 2 };
+            int result = solution.SingleNumber(nums);
+            Console.WriteLine(result);
+        }
+    }
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result ^= nums[i];
+            }
+            return result;
+        }
+    }
+
+}
