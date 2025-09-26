8491bcf36502012be7cb396bed97555cc8b4405d
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-26 21:17:57 +0300

    Solved Reverse Linked List Linked-List Problem

---
+++ Linked-List/Linked-List-ez-ReverseLinkedList/Linked-List-ez-ReverseLinkedList/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Linked_List_ez_ReverseLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var res = solution.ReverseList(new ListNode { val = 5,  next = null });
+            Console.WriteLine($"{res.val},{res.next}");
+        }
+    }
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode? prev = null;
+            ListNode curr = head;
+
+            while (curr != null)
+            {
+                ListNode temp = curr.next;
+                curr.next = prev!;
+                prev = curr;
+                curr = temp;
+            }
+            return prev!;
+        }
+    }
+
+}
