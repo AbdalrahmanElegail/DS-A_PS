3ade08c20bf0d93768949974aa09439cd9e5f54b
Abdalrahman <Abdalrahmanelegail@gmail.com>
2025-09-27 21:55:07 +0300

    Solved Best Time To Buy And Sell Stock Sliding_Window problem

---
+++ Sliding-Window/Sliding-Window-ez-BestTimeToBuyAndSellStock/Sliding-Window-ez-BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sliding_Window_ez_BestTimeToBuyAndSellStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] prices = [3, 2, 5, 6, 15, 5 ,1,4,5,6,7,8,6,5,10,11];
+            solution.MaxProfit(prices);
+        }
+    }
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int l = 0, r = 0, maxPrice = 0;
+
+            while (r < prices.Length)
+            {
+                if (prices[r] > prices[l])
+                    maxPrice = Math.Max(maxPrice, prices[r] - prices[l]);
+                else
+                    l = r;
+                r++;
+            }
+
+            return maxPrice;
+        }
+    }
+
+}
